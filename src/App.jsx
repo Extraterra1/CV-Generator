831d1b2d857a02212d8a34e8988035386984939b
Carlos Pires <extraterra1@gmail.com>
2023-10-23 13:02:23 +0100

    dev: replace iconoir with tabler

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
-  return (
-    <>
-      <h1>Hey</h1>
-    </>
-  );
+  return <Header />;
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,13 @@
+import { IconBrandTwitterFilled, IconBrandGithubFilled, IconBrandYoutubeFilled } from '@tabler/icons-react';
+import './Header.css';
+
 export default function Header() {
   return (
     <div className="header">
       <span className="title">CV Generator</span>
+      <IconBrandTwitterFilled />
+      <IconBrandGithubFilled />
+      <IconBrandYoutubeFilled />
     </div>
   );
 }
