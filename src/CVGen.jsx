0fd9944d1fec1697a9fcc2e7794939412b6f2a85
Carlos Pires <extraterra1@gmail.com>
2023-10-24 13:09:22 +0100

    refactor: inputs in state + uuid

--- src/CVGen.jsx
+++ src/CVGen.jsx
@@ -1,18 +1,39 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import './CVGen.css';
 import Input from './Input';
 
 export default function CVGen() {
   const [name, setName] = useState('');
-  const handleInputChange = (ev) => {
-    return setName(ev.target.value);
+  const handleInputChange = (ev, key) => {
+    console.log(ev, key);
+    // return setName(ev.target.value);
   };
+  const inputs = [
+    {
+      name: 'name',
+      type: 'text',
+      label: 'Full Name',
+      id: uuid()
+    },
+    {
+      name: 'phone',
+      type: 'tel',
+      label: 'Phone',
+      id: uuid()
+    }
+  ];
+
+  const inputElements = inputs.map((e) => {
+    return <Input name={e.name} type={e.type} label={e.label} key={e.id} handleInputChange={(ev) => handleInputChange(ev, e.id)} />;
+  });
+
   return (
     <div className="container">
       <div className="inputs-container">
         <div className="inputs">
           <h2 className="inputs-title">Your Info</h2>
-          <Input name="name" handleInputChange={handleInputChange} />
+          {inputElements}
         </div>
       </div>
       <div className="page-container">
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-export default function Input({ handleInputChange, name, type = 'text' }) {
+export default function Input({ handleInputChange, name, label, type = 'text' }) {
   return (
     <div className="form-group">
-      <label htmlFor={name}>Name</label>
+      <label htmlFor={name}>{label}</label>
       <input type={type} name={name} id={name} onChange={handleInputChange} />
     </div>
   );
