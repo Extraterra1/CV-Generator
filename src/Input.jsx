a26ca10ad7f9f18b4b937ce752867d0cf0549462
Carlos Pires <extraterra1@gmail.com>
2023-10-25 12:15:41 +0100

    feat: country select state

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,20 +1,37 @@
 /* eslint-disable react/prop-types */
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import Select from 'react-select';
 import countryList from 'react-select-country-list';
 import './Input.css';
 
 export default function Input({ handleInputChange, name, label, type = 'text' }) {
   const options = useMemo(() => countryList().getData(), []);
+  const [selectValue, setSelectValue] = useState('');
   const changeHandler = (value) => {
-    console.log(value);
+    handleInputChange({ target: { value: value.label } });
+    setSelectValue(value);
   };
 
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       {type !== 'select' && <input type={type} name={name} id={name} onChange={handleInputChange} />}
-      {type === 'select' && name === 'country' && <Select options={options} name={name} id={name} value="" onChange={changeHandler} />}
+      {type === 'select' && name === 'country' && (
+        <Select
+          options={options}
+          name={name}
+          id={name}
+          value={selectValue}
+          onChange={changeHandler}
+          className="countrySelect"
+          styles={{
+            option: (baseStyles, state) => ({
+              ...baseStyles,
+              color: '#242424'
+            })
+          }}
+        />
+      )}
     </div>
   );
 }
